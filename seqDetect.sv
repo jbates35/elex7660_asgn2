cedd66900bad29fab743381a6f1be172e010f0b6
Jimmy Bates <cbates21@my.bcit.ca>
2022-02-12 18:56:30 -0800

    Initial commit

---
+++ seqDetect.sv
@@ -0,0 +1,46 @@
+/********
+seqDetect.sv
+
+Written by Jimmy Bates (A01035957)
+
+ELEX 7660-Digital System Design
+Assignment 2
+
+Date created: February 12, 2022
+
+Bit shifts a bit in and checks to see if last 3 bits match input seq
+
+code for modelsim:
+vsim work.seqDetect_tb; add wave sim:*; run -all
+
+*********/
+
+module seqDetect #(parameter N=6) (
+        output logic valid,
+        input logic [N-1:0] seq,
+        input logic clk, reset_n, a
+        );
+
+    logic [N-1:0] lastNbits, lastNbits_next; //for storing last 3 bits
+    logic [N-1:0] bitCheck; //for checking seq with last bits
+
+    always_comb begin : bitshift
+        //Bit shift the lastNbits by 1 to the left
+        lastNbits_next = lastNbits << 1;
+        
+        //Assign LSB of lastNbits to a
+        lastNbits_next[0] = a;
+
+        //Valid must be all bits ANDED - lastNbits XNOR'd w seq
+        valid = &(lastNbits ~^ seq)
+    end : bitshift
+
+    always_ff @(posedge clk, negedge reset_n) begin : clkff
+        //Take next value on clk unless reset
+        if(~reset_n) 
+            lastNbits <= 'b000
+        else
+            lastNbits <= lastNbits_next
+    end : clkff
+
+endmodule
---
+++ seqDetect_tb.sv
@@ -0,0 +1,34 @@
+/********
+seqDetect.sv
+
+Written by Jimmy Bates (A01035957)
+
+ELEX 7660-Digital System Design
+Assignment 2
+
+Date created: February 12, 2022
+
+Test bench for sequence detector
+
+code for modelsim:
+vsim work.seqDetect_tb; add wave sim:*; run -all
+
+*********/
+
+//Instantiate model
+
+//Reset
+
+//Round 1
+//Store some set of bits in a 6 bit word
+//Setup correct input
+//Run clock 9 times
+
+//Reset
+
+//Round 2
+//Store some set of bits in a 6 bit word
+//Set up correct input
+//Run clock 9 times
+
+//stop
