aa5ca2d3db03b699709edf058a07def4b90c6927
Jimmy Bates <cbates21@my.bcit.ca>
2022-02-13 00:01:30 -0800

    Finished testbench

--- vendingMachine_tb.sv
+++ vendingMachine_tb.sv
@@ -20,22 +20,63 @@ vsim work.vendingMachine_tb; add wave sim:*; run -all
 
 module vendingMachine_tb;
 
-    //All input logic
-    logic valid,
-    logic nickel, dime, quarter,
-    logic clk, reset_n
+    logic valid, nickel, dime, quarter, clk, reset_n;
+    logic [2:0] coins; 
 
-    //Import assignments to logic
+    //initialize module
     vendingMachine uut (.*);
 
-    //Reset
+    initial begin
 
-    //Count up to 8, checking an input of each combination. 
+        //initialize clock and starting coin values
+        clk = '0;
+        coins = 3'b000;
 
-    //Reset
+        //Reset first
+        reset_n = 0;
+        @(posedge clk);
+        reset_n = 1;
+        @(posedge clk);
 
-    //Count up to 95
-    //Now add a nickel, dime and quarter at same time
-    //Should have 0 now, stop
+        //Test all combinations of coins
+        //Make sure when vending machine is >100 the next cycle vending machine
+        //doens't eat the coins
+        for (int i = 0; i <= 7; i++) begin : coinfor
+            coins = i;
+            @(posedge clk); 
+        end : coinfor
+
+        coins = 'b000;
+
+        //Reset again
+        reset_n = 0;
+        @(posedge clk);
+        reset_n = 1;
+        @(posedge clk);
+
+        //Now lets get coins to 95
+        coins = 'h7;
+        @(posedge clk);
+        coins = 'h7;
+        @(posedge clk);
+        coins = 'h3;
+        @(posedge clk);
+        coins = 'h7;
+        repeat(2) @(posedge clk);
+
+        //Simulation done
+        $stop;
+
+    end
+
+    always begin
+        //Assign coins to the diff coins
+        quarter = coins[2];
+        dime = coins[1];
+        nickel = coins[0];
+
+        //clk
+        #500ns clk = ~clk;
+    end
 
 endmodule
